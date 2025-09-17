2a50e5ac5982f3a1270ced4f50971a83f968502b
alexsisson <alexwsisson@gmail.com>
2025-09-17 17:23:55 -0500

    /

--- mission.js
+++ mission.js
@@ -1,4 +1,3 @@
-// JavaScript for theme switching
 const themeSelector = document.querySelector('#themeSelector');
 const body = document.body;
 const logo = document.querySelector('#logo');
@@ -6,12 +5,11 @@ const logo = document.querySelector('#logo');
 function changeTheme() {
   if (themeSelector.value === 'dark') {
     body.classList.add('dark');
-    logo.src = 'byui-logo-white.png';  // white logo file (same dimensions)
+    logo.src = 'byui-logo-white.png';  
   } else {
     body.classList.remove('dark');
-    logo.src = 'byui-logo_blue.webp';  // blue logo file (same dimensions)
+    logo.src = 'byui-logo_blue.webp';  
   }
 }
 
-// Add an event listener to the themeSelector
 themeSelector.addEventListener('change', changeTheme);
