85565734acfbd4c528edfc14629a36ee34d98cb0
alexsisson <alexwsisson@gmail.com>
2025-09-18 10:58:06 -0500

    /

--- mission.js
+++ mission.js
@@ -5,7 +5,7 @@ const logo = document.querySelector('#logo');
 function changeTheme() {
   if (themeSelector.value === 'dark') {
     body.classList.add('dark');
-    logo.src = 'byui-logo-white.png';  
+    logo.src = 'byui-logo_white.png';  
   } else {
     body.classList.remove('dark');
     logo.src = 'byui-logo_blue.webp';  
